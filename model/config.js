9af86c1559ec81cc027eb6e0433980134ee4feb7
Yumi <caubedondoc.ls01@gmail.com>
2019-01-12 19:14:28 +0700

    Update config.js

--- model/config.js
+++ model/config.js
@@ -2,5 +2,6 @@ module.exports = {
     token: process.env.token,
     prefix: "...",
     ownerid: process.env.owner,
-    redis: process.env.redis
+    redis: process.env.redis,
+    test:"test?"
 }
